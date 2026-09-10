a4689ece032647d94bee8518c20feadac59867b8
SNprog-art <84011702+SNprog-art@users.noreply.github.com>
2026-09-11 04:05:53 +0700

    4.6.7_1_Построить аппроксимацию МНК.py

---
+++ Lecture_4/1_Build an approximation LSM.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Чтение исходных данных
+# -------------------------
+import re
+file = open("data.txt")
+values = file.read().split("\n")
+data = []
+for key in values:
+    value = re.findall(r"[-+]?\d*\.\d+|\d+", key)
+    
+    if value != []:
+        data.append(value)
+# -------------------------
+x = np.zeros(11)
+y = np.zeros(11)
+for i in range(0,11):
+    x[i] = data[i][0]
+    y[i] = data[i][1]
+
+print("Считанные данные из файла: \n", data)
+print("Вектор значений x: \n",x)
+print("Вектор значений y(x): \n",y)
+
+x_intp = np.zeros(101)
+n_x_intp = np.size(x_intp)
+for i in range(0,n_x_intp-1):
+    x_intp[i+1] = x_intp[i] + 0.1
+
+y_intp = np.zeros(n_x_intp)
+
+# Аппроксимация данных методом наименьших квадратов
+# на основе функции полифит библиотеки NumPy
+koeff = np.polyfit(x, y, 4)
+print("Коэффициенты полинома", koeff)
+for i in range(0, n_x_app):
+    y_app[i] = koeff[0] * pow(x_app[i], 4) + koeff[1] * pow(x_app[i], 3) + koeff[2] * pow(x_app[i], 2) + koeff[3] * pow(x_app[i], 1) + koeff[4]
